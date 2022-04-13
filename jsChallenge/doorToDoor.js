943f5ae4600125252d50b3d768850570cba705e0
charuzain <charuabhizain@gmail.com>
2022-04-12 22:31:18 -0400

    Day 1: Door to door challenge

--- jsChallenge/doorToDoor.js
+++ jsChallenge/doorToDoor.js
@@ -1,6 +1,6 @@
 /*********************************************************************
                    Door To Door Challenge
-                ********************************
+ *********************************************************************
 Instructions
 Given an array of volunteer names and an array of neighbourhood names, complete the doorToDoor function so that it returns the number of neighbourhoods each volunteer should visit if the work of going door to door is split evenly amongst them.
 
