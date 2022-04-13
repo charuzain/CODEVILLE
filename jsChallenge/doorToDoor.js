aac67ebac23b7aeda4888eef4f33133da415c381
charuzain <charuabhizain@gmail.com>
2022-04-12 22:29:49 -0400

    21 day javascript coding challenge

---
+++ jsChallenge/doorToDoor.js
@@ -0,0 +1,53 @@
+/*********************************************************************
+                   Door To Door Challenge
+                ********************************
+Instructions
+Given an array of volunteer names and an array of neighbourhood names, complete the doorToDoor function so that it returns the number of neighbourhoods each volunteer should visit if the work of going door to door is split evenly amongst them.
+
+Examples
+Input:
+
+const volunteers = [
+  'Sally',
+  'Jake',
+  'Brian',
+  'Hamid'
+];
+const neighbourhoods = [
+  'Central Valley',
+  'Big Mountain',
+  'Little Bridge',
+  'Bricktown',
+  'Brownsville',
+  "Paul's Boutique",
+  'Clay Park',
+  'Fox Nest'
+];
+Output:
+2
+
+**********************************************************************
+                          Solution
+*********************************************************************/
+const doorToDoor = (volunteers,neighbourhoods)=>{
+  return neighbourhoods.length / volunteers.length;
+};
+
+const volunteers = [
+  'Sally',
+  'Jake',
+  'Brian',
+  'Hamid'
+];
+const neighbourhoods = [
+  'Central Valley',
+  'Big Mountain',
+  'Little Bridge',
+  'Bricktown',
+  'Brownsville',
+  "Paul's Boutique",
+  'Clay Park',
+  'Fox Nest'
+];
+
+console.log(doorToDoor(volunteers,neighbourhoods));
